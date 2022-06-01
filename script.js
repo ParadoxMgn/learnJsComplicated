7580a93637592a410270d95693dcf30087e054a3
Eugene Shumkov <radoxmgn@gmail.com>
2022-06-01 12:38:54 +0300

    task of the lesson05

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 'use strict';
 
-const myFuction = function (someArg) {
-  if (typeof someArg === "string") {
-    const cutSomeArg = someArg.trim();
-    if (cutSomeArg.length > 30) {
-      return cutSomeArg.substring(0, 30) + "...";
-    } else {
-      return cutSomeArg;
-    }
-  } else {
-    return 'Это не строка';
-  }
-};
+const arr = [
+  '4245453',
+  '8678565345',
+  '3645634535',
+  '2453544353',
+  '657575354',
+  '96759545345',
+  '2352434535',
+];
 
-console.log(myFuction('         Lorem ipsum dolor, sit amet consectetur adipisicing elit.         '));
+for (let item of arr) {
+  if (item[0] === '2' || item[0] === '4') {
+    console.log(item);
+  }
+}
+
+for (let num = 2; num <= 100; num++) {
+  let isSimpleNum = true;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isSimpleNum = false;
+      break;
+    }
+  }
+  if (isSimpleNum) {
+    console.log(num, ('Делители этого числа 1 и ' + num));
+  }
+}
