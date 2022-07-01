d2085922c7e1787a630220daa7deae3bbe9dc548
Eugene Shumkov <radoxmgn@gmail.com>
2022-07-01 12:54:00 +0300

    first commit lesson26

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
 'use strict';
 
-const inputText = document.getElementById('input-text');
-const text = document.getElementById('text');
 
-const debounce = (func, ms) => {
-  let indexTimeout;
-  return () => {
-    clearTimeout(indexTimeout);
-    indexTimeout = setTimeout(func, ms);
-  };
-};
-
-const inputletter = () => {
-  text.innerText = inputText.value;
-};
-
-inputText.addEventListener('input', debounce(inputletter, 300));
